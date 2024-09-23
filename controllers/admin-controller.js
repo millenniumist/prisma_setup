576dbad26e8209b433ec2a72360c3645244fc06a
Millenniumist <suparit.panta@gmail.com>
2024-09-23 18:13:53 +0700

    clean files

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,56 +1,58 @@
-const {createProductSchema} = require("../validators/product");
+const { createProductSchema } = require("../validators/product");
 const cloudinary = require("../config/cloudinary");
 const prisma = require("../config/prisma");
 const fs = require("fs/promises");
-exports.adminCreateProduct = async(req, res, next) => {
-    try {
-        console.log(req.body)
-        const data = await createProductSchema.validateAsync(req.body);
-        console.log(data)
-        const imagePromiseArray = []
-        for(let file of files) {
-            const promiseUrl = cloudinary.uploader.upload(file.path);
-            imagePromiseArray.push(promiseUrl);
-        }
-        const imageArray = await Promise.all(imagePromiseArray);
-        const newProduct = await prisma.product.create({
-            data: {
-                ...data,
-                productImages: {
-                    createMany: {
-                        data: imageArray.map((el)=> ({url: el.secure_url}))
-                    }
-                }
-            },include: {
-                productImages: true
-            }
-        })
-        res.json({ message: "create product success" });
-    } catch (error) {
-        next(error);
-    } finally {
-     const deleteFiles =  req.files.map(file => fs.unlink(file.path));
-     await Promise.all(deleteFiles);
-}
+exports.adminCreateProduct = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const data = await createProductSchema.validateAsync(req.body);
+    console.log(data);
+    const imagePromiseArray = [];
+    for (let file of files) {
+      const promiseUrl = cloudinary.uploader.upload(file.path);
+      imagePromiseArray.push(promiseUrl);
+    }
+    const imageArray = await Promise.all(imagePromiseArray);
+    const newProduct = await prisma.product.create({
+      data: {
+        ...data,
+        productImages: {
+          createMany: {
+            data: imageArray.map((el) => ({ url: el.secure_url })),
+          },
+        },
+      },
+      include: {
+        productImages: true,
+      },
+    });
+    res.json({ message: "create product success" });
+  } catch (error) {
+    next(error);
+  } finally {
+    const deleteFiles = req.files.map((file) => fs.unlink(file.path));
+    await Promise.all(deleteFiles);
+  }
+};
 exports.adminUpdateProduct = (req, res, next) => {
-    try {
-        res.json({ message: "update product success" });
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    res.json({ message: "update product success" });
+  } catch (error) {
+    next(error);
+  }
+};
 exports.adminDeleteProduct = (req, res, next) => {
-    try {
-        res.json({ message: "delete product success" });
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    res.json({ message: "delete product success" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.adminUpdateOrderStatus = (req, res, next) => {
-    try {
-        res.json({ message: "update order status success" });
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    res.json({ message: "update order status success" });
+  } catch (error) {
+    next(error);
+  }
+};
