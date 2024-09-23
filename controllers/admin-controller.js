6c6ee13f8d239bbd6c8a7475ad4045c2a2c3e585
Millenniumist <suparit.panta@gmail.com>
2024-09-23 18:00:35 +0700

    cloudinary

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,9 +1,16 @@
 const {createProductSchema} = require("../validators/product");
+const cloudinary = require("../config/cloudinary");
 exports.adminCreateProduct = async(req, res, next) => {
     try {
         console.log(req.body)
         const data = await createProductSchema.validateAsync(req.body);
         console.log(data)
+        const imagePromiseArray = []
+        for(let file of files) {
+            const promiseUrl = cloudinary.uploader.upload(file.path);
+            imagePromiseArray.push(promiseUrl);
+        }
+        const imageArray = await Promise.all(imagePromiseArray);
         res.json({ message: "create product success" });
     } catch (error) {
         next(error);
